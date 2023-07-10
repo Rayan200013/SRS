8a192d44b1ac018935e092c7d1a00146bb21bd49
SalamAbouShakra <salam.abu.shakra@gmail.com>
2023-07-10 19:34:01 +0300

    Salam's commit

--- Salam.js
+++ Salam.js
@@ -5,7 +5,7 @@ class Book {
       this.pages = pages
     }
     read() {
-        const saying = 'You have started reading'+ this.title + 'by' + this.author + '. It has '+ this.pages + 'pages.'
+        const saying = 'You have started reading '+ this.title + ' by ' + this.author + '. It has '+ this.pages + ' pages.'
         return saying
       }
   }
@@ -13,3 +13,5 @@ class Book {
   const book1 = new Book('Wonders of Assignment 1', 'Salam', 300)
   
   console.log(book1.read())
+
+
